a34df8e31ea1d8323eb0b6138a527f66cc179e6d
profanta3 <42981287+profanta3@users.noreply.github.com>
2022-04-22 10:56:24 +0200

    KP-79 initialized starter Projekt

---
+++ src/main/java/de/htwkbeg4/WarehouseService/WarehouseServiceApplication.java
@@ -0,0 +1,13 @@
+package de.htwkbeg4.WarehouseService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseServiceApplication.class, args);
+	}
+
+}
