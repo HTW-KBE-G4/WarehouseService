91a3b7f53134f23f6dc6c6a56f10a0f717443539
profanta3 <42981287+profanta3@users.noreply.github.com>
2022-04-26 12:41:56 +0200

    KP-36 fixed csv reading bug when packaged as jar insode docker and updated model class

--- src/main/java/de/htwkbeg4/WarehouseService/data/CSVDataReader.java
+++ src/main/java/de/htwkbeg4/WarehouseService/data/CSVDataReader.java
@@ -1,15 +1,32 @@
 package de.htwkbeg4.WarehouseService.data;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.enums.CSVReaderNullFieldIndicator;
 import de.htwkbeg4.WarehouseService.model.PCComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CSVDataReader {
+
+    private ResourceLoader resourceLoader;
+
+    CSVDataReader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
     /**
      * Reads data from file
      * @param path path to file
@@ -20,12 +37,24 @@ public class CSVDataReader {
         List<String[]> list = new ArrayList<>();
 
         try {
-            FileReader fileReader = new FileReader(new ClassPathResource(path).getFile());
+            //FileReader fileReader = new FileReader(new ClassPathResource(path).getInputStream().toString());
 
-            CSVReader csvReader = new CSVReader(fileReader);
+            Resource resource = resourceLoader.getResource(path);
+            InputStream inputStream = resource.getInputStream();
+
+
+            CSVParser parser = new CSVParserBuilder()
+                    .withSeparator(';')
+                    .build();
+
+            CSVReader csvReader = new CSVReaderBuilder(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                    .withCSVParser(parser)
+                    .withSkipLines(1)
+                    .build();
 
             list = csvReader.readAll();
-            fileReader.close();
+            inputStream.close();
+            //fileReader.close();
             csvReader.close();
         }
         catch (Exception e) {
@@ -47,7 +76,7 @@ public class CSVDataReader {
 
         for (String[] comp :
                 list) {
-            objects.add(new PCComponent(comp[0], comp[1], comp[2], comp[3], comp[4]));
+            objects.add(new PCComponent(Long.getLong(comp[0]), comp[1], comp[2], comp[3], comp[4], comp[5], comp[6], comp[7], comp[8], comp[9], comp[10]));
         }
 
         return objects;
